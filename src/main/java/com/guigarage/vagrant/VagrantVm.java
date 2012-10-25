f2f6826af62ab93e0a3155182b107f95d85b6c2d
Hendrik Ebbers <hendrik.ebbers@web.de>
2012-10-25 23:41:25 +0200

    refactoring

--- src/main/java/com/guigarage/vagrant/VagrantVm.java
+++ src/main/java/com/guigarage/vagrant/VagrantVm.java
@@ -86,9 +86,4 @@ public class VagrantVm {
 	public String getUuid() {
 		return ((RubyObject) vagrantVm.callMethod("uuid")).toString();
 	}
-	
-	public String getIpAdress() {
-		RubyHash hash = (RubyHash) ((RubyObject) vagrantVm.callMethod("ssh")).callMethod("info");
-		return "";
-	}
 }
